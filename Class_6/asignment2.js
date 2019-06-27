c70223517d912bfdbbd5090287cc5e942ef3d71f
Shirdi Nagaraju Mudunuri <Mudunuri@civicplus.com>
2019-06-27 18:16:03 -0500

    Assignments for class 6

---
+++ Class_6/asignment2.js
@@ -0,0 +1,16 @@
+function clearDups(inpArray) {
+  let clearedDupsArray = [];
+  let lenOfArray = inpArray.length;
+  for (let i = 0; i < lenOfArray - 1; ++i) {
+    if (inpArray[i] == inpArray[i + 1]) {
+      continue;
+    }
+    clearedDupsArray.push(inpArray[i]);
+  }
+  if (
+    clearedDupsArray[clearedDupsArray.length - 2] != inpArray[lenOfArray - 1]
+  ) {
+    clearedDupsArray.push(inpArray[lenOfArray - 1]);
+  }
+  return clearedDupsArray;
+}
