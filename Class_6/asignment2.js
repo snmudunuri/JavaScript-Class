3eba1e469602bd93a2c3d085a7c9f87401ee9156
Shirdi Nagaraju Mudunuri <Mudunuri@civicplus.com>
2019-07-01 10:02:38 -0500

    Made changes to logic

--- Class_6/asignment2.js
+++ Class_6/asignment2.js
@@ -1,14 +1,14 @@
 function clearDups(inpArray) {
   let clearedDupsArray = [];
   let lenOfArray = inpArray.length;
-  for (let i = 0; i < lenOfArray - 1; ++i) {
+  for (let i = 0; i < lenOfArray - 1; i++) {
     if (inpArray[i] == inpArray[i + 1]) {
       continue;
     }
     clearedDupsArray.push(inpArray[i]);
   }
   if (
-    clearedDupsArray[clearedDupsArray.length - 2] != inpArray[lenOfArray - 1]
+    clearedDupsArray[clearedDupsArray.length - 1] != inpArray[lenOfArray - 1]
   ) {
     clearedDupsArray.push(inpArray[lenOfArray - 1]);
   }
